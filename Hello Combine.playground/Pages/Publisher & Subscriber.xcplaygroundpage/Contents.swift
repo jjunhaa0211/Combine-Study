7ede093180c88f8ea19397cea1c22c2f51ee9f29
junha <goodjunha@gmail.com>
2022-08-10 00:35:09 +0900

    ADD:: Publisher & Subscriber 배운 내용

---
+++ Hello Combine.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [Previous](@previous)
+
+import Foundation
+import Combine
+
+// Publisher & Subscriber
+let just = Just(1000) // Just = 데이터를 한번 전송하고 끝
+let subscription1 = just.sink(receiveValue: {value in
+    print("Received Value: \(value)")
+})
+
+//Received Value: 1000
+
+let arrayPublisher = [1, 3, 5, 7, 9].publisher
+let subscription2 = arrayPublisher.sink(receiveValue: { value in
+    print("Received Value: \(value)")
+})
+
+//Received Value: 1
+//Received Value: 3
+//Received Value: 5
+//Received Value: 7
+//Received Value: 9
+
+class MyClass {
+    var property: Int = 0 {
+        didSet{
+            print("Did set property to \(property)")
+        }
+    }
+}
+
+let object = MyClass()
+let subscription3 = arrayPublisher.assign(to: \.property, on: object)
+print("Final Value: \(object.property)")
+
+
+
+
+
+//: [Next](@next)
+
+
